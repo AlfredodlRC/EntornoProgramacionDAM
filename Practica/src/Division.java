a2c31c841cab985220a0b3ae1bae0801409f225a
Alfredo de los Rios <dixie_flatline2002@hotmail.com>
2021-02-11 18:35:17 +0100

    Inicial con clase Division creada

---
+++ Practica/src/Division.java
@@ -0,0 +1,93 @@
+
+
+/**
+ * 
+ * @author Alfredo de los Rios Carrizo
+ * @version 1.0
+ * 
+ *  Clase que realiza operaciones relaccionadas con la división 
+ *
+ */
+
+public class Division {
+
+	
+	
+	//
+	// Atributo propiedad para las clases suma,resta y producto con un
+	// contructor para poder inicializarlo
+	//
+	
+	//private double acumulador;
+	
+	//public Division(double acumulador) {
+	//	super();
+	//	this.acumulador = acumulador;		
+	//}
+	
+	
+	/**
+	 * Contructor sin parametros de la clase
+	 */
+	public Division() {
+		super();
+	}
+
+	/**
+	 * Método que realiza la división entre dos números enteros que devuelve
+	 *  otro número entero obviando la parte fraccionaria.
+	 * 
+	 * @param denominador -> parametro que será dividido por el segundo parametro.
+	 * @param divisor -> parametro que divide al primer parametro.
+	 * @return devuelve la parte entera de la división de los dos parametros.
+	 */
+	public long dividir(int denominador, int divisor) {
+		long resultado = 0;
+		
+		return resultado;
+	}
+	
+	/**
+	 * Método que realiza la división entre dos números reales que devuelve
+	 *  otro número real incorporando la parte fraccionaria.
+	 *  
+	 * @param denominador -> parametro que será dividido por el segundo parametro. 
+	 * @param divisor -> parametro que divide al primer parametro.
+	 * @return resultado de la división de los 2 parametros de entrada.
+	 */
+	public double dividir(double denominador, double divisor) {
+		double resultado = 0;
+		
+		return resultado;
+	}
+
+	/**
+	 * Método que calcula el número inverso (1/n) del número dado como parametro.
+	 * Devuelve un número real.
+	 * 
+	 * @param numero -> parametro del cual calcularemos el inverso.
+	 * @return devuelve el inverso del parametro de entrada (1 / numero).
+	 */
+	public double inverso(double numero) {
+		double resultado = 0;
+		
+		return resultado;
+	}
+	
+	/**
+	 * Método que calcula la raiz cuadrada del número dado como parametro.
+	 * Devuelve un número real.
+	 * 
+	 * Calcula la raiz cuadrada del parametro de entrada.
+	 * @param numero -> parametro al cual aplicar la raiz cuadrada.
+	 * @return devuelve la raiz cuadrada del parametro introducido.
+	 */
+	public double raiz(double numero) {
+		double resultado = 0;
+		
+		return resultado;
+	}
+	
+	
+	
+}
